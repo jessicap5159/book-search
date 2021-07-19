f520d06da42e49b83f75c155426612da3de2d848
jessicap5159 <jessicaj5159@gmail.com>
2021-07-18 19:06:04 -0700

    fixing bugs

--- client/src/App.js
+++ client/src/App.js
@@ -16,12 +16,6 @@ const httpLink = createHttpLink({
   uri: '/graphql',
 });
 
-
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
-
 const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem('id_token');
   return {
@@ -31,6 +25,13 @@ const authLink = setContext((_, { headers }) => {
     },
   };
 });
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+
 
 function App() {
   return (
--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -2,10 +2,9 @@
 import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../utils/mutations';
-
 import { Form, Button, Alert } from 'react-bootstrap';
 
-
+// import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
@@ -52,7 +51,7 @@ const LoginForm = () => {
     <>
       <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
         <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
-          Something went wrong with your login credentials!
+        {error && <div>Login failed</div>}
         </Alert>
         <Form.Group>
           <Form.Label htmlFor='email'>Email</Form.Label>
