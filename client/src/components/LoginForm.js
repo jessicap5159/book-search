43c4d8d304c4800b476331122ec08180b94e0b71
jessicap5159 <jessicaj5159@gmail.com>
2021-07-18 17:07:47 -0700

    fixing more errors in loginform.js

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -35,7 +35,7 @@ const LoginForm = () => {
         variables: { ...userFormData }
       });
 
-      Auth.login(token);
+      Auth.login(data.login.token)
     } catch (err) {
       console.error(err);
       setShowAlert(true);
